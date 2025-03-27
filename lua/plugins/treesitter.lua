ec496a54ae18323ce56541ead8cb419f9c1575d4
Aydin Aksel <154738769+aydinaksel@users.noreply.github.com>
2025-03-27 01:01:24 +0000

    Treesitter changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,11 +5,16 @@ return {
     config = function()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-        ensure_installed = { "lua", "rust", "apex", "html", "css", "python", "javascript", },
+        ensure_installed = {
+          "soql", "sosl", "sql", "lua", "rust", "apex", "html", "css",
+          "python", "javascript",
+        },
+        modules = {},
+        ignore_install = {},
+        indent = { enable = false },
         sync_install = false,
         auto_install = false,
         highlight = { enable = true },
-        indent = { enable = true },
       })
     end
   }
