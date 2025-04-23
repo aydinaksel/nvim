472bba7a57d724135f6a28f5fde6deb5fc8b57c4
Aydin Aksel <154738769+aydinaksel@users.noreply.github.com>
2025-04-23 11:54:23 +0100

    Various changes to LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,6 +17,7 @@ return {
       local capabilities = require("blink.cmp").get_lsp_capabilities()
 
       lspconfig.gopls.setup { capabilities = capabilities }
+      lspconfig.html.setup { capabilities = capabilities }
       lspconfig.ruff.setup { capabilities = capabilities }
       lspconfig.lua_ls.setup { capabilites = capabilities }
       lspconfig.quick_lint_js.setup { capabilites = capabilities }
@@ -34,18 +35,15 @@ return {
 
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
-          local c = vim.lsp.get_client_by_id(args.data.client_id)
-          if not c then return end
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
 
-          if vim.bo.filetype == "lua" then
-            -- Format the current buffer on save
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
-              end,
-            })
-          end
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+            end,
+          })
         end,
       })
     end,
