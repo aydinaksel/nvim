4ef56a0c34a53c245c29f08f0ff0da44d44a8d97
Aydin Aksel <154738769+aydinaksel@users.noreply.github.com>
2026-03-09 12:52:17 +0000

    Clipboard usage for headless server

--- lua/aydin/remap.lua
+++ lua/aydin/remap.lua
@@ -1,2 +1,16 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+if os.getenv("SSH_TTY") or os.getenv("SSH_CONNECTION") then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
